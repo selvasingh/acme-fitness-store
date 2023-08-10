23e6018a18b3e9cbbdda0c37871f6e9e31c9c543
Dingmeng Xue <dixue@microsoft.com>
2023-08-10 21:40:05 +0800

    update indentation

--- ai/acme-assist/src/main/java/com/azure/acme/assist/model/AcmeChatResponse.java
+++ ai/acme-assist/src/main/java/com/azure/acme/assist/model/AcmeChatResponse.java
@@ -11,4 +11,22 @@ public class AcmeChatResponse {
      * The candidate answers for the chat. Only one is provided for now.
      */
     private List<String> messages;
+
+    /**
+     * Set messages
+     * 
+     * @param messages
+     */
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+
+    /**
+     * Get messages
+     * 
+     * @return
+     */
+    public List<String> getMessages() {
+        return this.messages;
+    }
 }
