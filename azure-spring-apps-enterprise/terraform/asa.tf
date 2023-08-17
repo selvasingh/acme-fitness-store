8a565c0c6b3c53e169e2aeb783acdeaab8bbb984
Anish K. Patel <anishs dot online dot bills at gmail.com>
2023-08-16 20:49:35 -0400

    move providers to file, lock tf version

--- azure-spring-apps-enterprise/terraform/asa.tf
+++ azure-spring-apps-enterprise/terraform/asa.tf
@@ -1,23 +1,3 @@
-# Configure the Microsoft Azure Provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-  backend "azurerm" {
-  }
-}
-
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
-
 locals {
   azure-metadeta = "azure.extensions"
 }
